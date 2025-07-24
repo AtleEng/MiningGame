9c9886e3fb09fa21ffd44e0ed7f1607f14f0000e
AtleEng <atle.engelbrektsson@gmail.com>
2025-07-24 15:06:33 +0200

    Added build functions

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+static bool running = true;
+
+int main()
+{
+    cout << "Hello World!";
+    while (running)
+    {
+        /* code */
+    }
+    
+    return 0;
+}
