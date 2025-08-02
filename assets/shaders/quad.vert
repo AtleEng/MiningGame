7d097defe537114adf07ad02632a0028f4fe7fc3
AtleEng <atle.engelbrektsson@gmail.com>
2025-08-02 19:57:16 +0200

    added image rendering

--- assets/shaders/quad.vert
+++ assets/shaders/quad.vert
@@ -1,5 +1,10 @@
 #version 430 core
 
+//Input
+
+//Output
+layout (location = 0) out vec2 textureCoordsOut;
+
 void main()
 {
     vec2 vertices[6] =
@@ -12,5 +17,21 @@ void main()
         vec2(0.5, -0.5),
     };
 
+    float left = 0.0;
+    float top = 0.0;
+    float right = 16.0;
+    float bottom = 16.0;
+
+    vec2 textureCoords[6] =
+    {
+        vec2(left,top),
+        vec2(left,bottom),
+        vec2(right,top),
+        vec2(right,top),
+        vec2(left,bottom),
+        vec2(right,bottom),
+    };
+
     gl_Position = vec4(vertices[gl_VertexID], 1.0, 1.0);
+    textureCoordsOut = textureCoords[gl_VertexID];
 }
--- src/gl_renderer.cpp
+++ src/gl_renderer.cpp
@@ -1,30 +1,38 @@
 #include "gl_renderer.h"
 
+// To load PNG
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+// ################################     OpenGL Constants    ################################
+const char *TEXTURE_PATH = "assets/textures/Texture_Atlas.png";
+
 // ################################     OpenGL Structs    ################################
 struct GLContext
 {
     GLuint programID;
+    GLuint textureID;
 };
 // ################################     OpenGL Globals    ################################
 static GLContext glContext;
 
 // ################################     OpenGL Functions    ################################
 static void APIENTRY gl_debug_callback(GLenum source, GLenum type, GLuint id, GLenum severity,
-                                         GLsizei length, const GLchar* message, const void* user)
+                                       GLsizei length, const GLchar *message, const void *user)
 {
-  if(severity == GL_DEBUG_SEVERITY_LOW || 
-     severity == GL_DEBUG_SEVERITY_MEDIUM ||
-     severity == GL_DEBUG_SEVERITY_HIGH)
-  {
-    SM_ASSERT(false, "OpenGL Error: %s", message);
-  }
-  else
-  {
-    SM_TRACE((char*)message);
-  }
+    if (severity == GL_DEBUG_SEVERITY_LOW ||
+        severity == GL_DEBUG_SEVERITY_MEDIUM ||
+        severity == GL_DEBUG_SEVERITY_HIGH)
+    {
+        SM_ASSERT(false, "OpenGL Error: %s", message);
+    }
+    else
+    {
+        SM_TRACE((char *)message);
+    }
 }
 
-bool gl_init(BumpAllocator* transientStorage)
+bool gl_init(BumpAllocator *transientStorage)
 {
     load_gl_functions();
 
@@ -36,10 +44,10 @@ bool gl_init(BumpAllocator* transientStorage)
     GLuint fragShaderID = glCreateShader(GL_FRAGMENT_SHADER);
 
     int fileSize = 0;
-    char* vertShader = read_file("assets/shaders/quad.vert", &fileSize, transientStorage);
-    char* fragShader = read_file("assets/shaders/quad.frag", &fileSize, transientStorage);
+    char *vertShader = read_file("assets/shaders/quad.vert", &fileSize, transientStorage);
+    char *fragShader = read_file("assets/shaders/quad.frag", &fileSize, transientStorage);
 
-    if(!vertShader || !fragShader)
+    if (!vertShader || !fragShader)
     {
         SM_ASSERT(false, "Failed to load shaders");
         return false;
@@ -51,25 +59,25 @@ bool gl_init(BumpAllocator* transientStorage)
     glCompileShader(vertShaderID);
     glCompileShader(fragShaderID);
 
-    //Test vert shaders
+    // Test vert shaders
     {
         int success;
         char shaderLog[2048] = {};
 
         glGetShaderiv(vertShaderID, GL_COMPILE_STATUS, &success);
-        if(!success)
+        if (!success)
         {
             glGetShaderInfoLog(vertShaderID, 2048, 0, shaderLog);
             SM_ASSERT(false, "Failed to compile vertex shaders %s", shaderLog);
         }
     }
-    //Test frag shaders
+    // Test frag shaders
     {
         int success;
         char shaderLog[2048] = {};
 
         glGetShaderiv(fragShaderID, GL_COMPILE_STATUS, &success);
-        if(!success)
+        if (!success)
         {
             glGetShaderInfoLog(fragShaderID, 2048, 0, shaderLog);
             SM_ASSERT(false, "Failed to compile fragment shaders %s", shaderLog);
@@ -86,12 +94,45 @@ bool gl_init(BumpAllocator* transientStorage)
     glDeleteShader(vertShaderID);
     glDeleteShader(fragShaderID);
 
-    //This has to be done so GL will draw
+    // This has to be done so GL will draw
     GLuint VAO;
     glGenVertexArrays(1, &VAO);
     glBindVertexArray(VAO);
 
-    //Depth Testing
+    // Texture Loading using STB image
+    {
+        int width, height, channels;
+        char *data = (char *)stbi_load(TEXTURE_PATH, &width, &height, &channels, 4);
+        if (!data)
+        {
+            SM_ASSERT(false, "Failed to load texture :(");
+            return false;
+        }
+
+        glGenTextures(1, &glContext.textureID);
+        glActiveTexture(GL_TEXTURE0);
+
+        glBindTexture(GL_TEXTURE_2D, glContext.textureID);
+
+        // set the texture wrapping/filtering options (on the currently bound texture object)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
+        // This setting only matters when using the GLSL texture() function
+        // When you use texelFetch() this setting has no effect,
+        // because texelFetch is designed for this purpose
+        // See: https://interactiveimmersive.io/blog/glsl/glsl-data-tricks/
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_SRGB8_ALPHA8, width, height,
+                     0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+
+        stbi_image_free(data);
+    }
+
+    glEnable(GL_FRAMEBUFFER_SRGB);
+    glDisable(0x809D);
+    // Depth Testing
     glEnable(GL_DEPTH_TEST);
     glDepthFunc(GL_GREATER);
 
@@ -102,11 +143,11 @@ bool gl_init(BumpAllocator* transientStorage)
 
 void gl_render()
 {
-    glClearColor(119.0f/255.0f, 33.0f/255.0f, 111.0f/255.0f, 1.0f);
+    glClearColor(119.0f / 255.0f, 33.0f / 255.0f, 111.0f / 255.0f, 1.0f);
     glClearDepth(0.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    glViewport(0,0, input.screenSizeX, input.screenSizeY);
+    glViewport(0, 0, input.screenSizeX, input.screenSizeY);
 
     glDrawArrays(GL_TRIANGLES, 0, 6);
 }
